9091fa3a8a9d4dc32f0ddb4abd5493377561628d
zkevinbai <lkevin957@gmail.com>
2019-01-18 15:27:32 -0800

    Finish hashing

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -1,14 +1,35 @@
+# uniform
+# deterministic
+# one way
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
+  MAGIC_NUMBER = 666
   def hash
+    return 0 if self.empty?
+    holder = []
+    self.each_index do |i|
+      if i < self.length-1
+        xored = self[i].object_id ^ self[i+1].object_id
+        added = self[i].object_id + self[i+1].object_id
+        multiply = self[i].object_id * self[i+1].object_id
+        combo = xored ^ added ^ multiply
+        holder << combo
+      else 
+        xored = self[i].object_id ^ MAGIC_NUMBER.object_id
+        holder << xored
+      end
+    end
+    holder.reduce {|acc, el| acc ^ el }
   end
 end
 
+
 class String
   def hash
+    arr = self.chars.to_a.map {|char| char.ord}.hash
   end
 end
 
@@ -16,6 +37,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    # 0
+    mykeys = self.keys.map {|char| char.ord unless char.class == Symbol}
+    myvals = self.values.map {|char| char.ord}
+    both = mykeys.sort + myvals.sort
+    both.hash
   end
 end
